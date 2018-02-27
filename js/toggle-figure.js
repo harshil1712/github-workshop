fffd4611a01686d113e2ce9a49107533d41ea33f
Gwen Lofman <Gwen@Lofman.io>
2018-02-27 12:49:07 -0500

    Port presntation to flexible reveal framework
    Supports splitting each slide into its own file; this significantly
    simplifies the process of updating and adding content to the
    presentation.
    
    Previously:
    - changes which affected slide order were hard to follow
    - changes which changed slide order and content for a related
      section were hard to follow
    - updating the theme was very manual
    - manually editing html files was very tedious
    
    Now:
    - support for either html or markdown, whichever works for a
      particular situation
    - allows changes which affect slide order to be totally separate
      from slide content
    - allows changes which change slide attributes to be totally
      separate from changes which affect slide content
    - much easier to write slide notes
    
    The reveal generator does, however, make it more difficult to
    keep a copy of the slides available online, and will require a
    separate GitHub repository to be kept online.

--- js/toggle-figure.js
+++ js/toggle-figure.js
@@ -1,20 +1,21 @@
-let figs = document.body
+window.addEventListener('load', function() {
+    let figs = document.body
     .querySelectorAll('.toggle-figure')
 
-for (let f=0; f < figs.length; f++ ) {
-    figs[f].addEventListener("click", () => {
-        figs[f].classList.toggle('toggle-figure--expand')
+    for (let f=0; f < figs.length; f++ ) {
+        figs[f].addEventListener("click", () => {
+            figs[f].classList.toggle('toggle-figure--expand')
 
-        figs[f].querySelector('.toggle-figure__button')
+            figs[f].querySelector('.toggle-figure__button')
             .classList.toggle('toggle_figure__button--expand')
 
-        figs[f].querySelector('.toggle-figure__figure')
+            figs[f].querySelector('.toggle-figure__figure')
             .classList.toggle('toggle_figure__figure--expand')
 
-        document.getElementById('body').style.display = 'none'
-        document.getElementById('body').style.display = 'inherit'
+            document.getElementById('body').style.display = 'none'
+            document.getElementById('body').style.display = 'inherit'
 
-
-        console.log('toggle figure %s', figs[f])
-    })
-}
+            console.log('toggle figure %s', figs[f])
+        })
+    }
+})
---
+++ slides/github/repo/new-dialog.md
@@ -0,0 +1,7 @@
+![new repository github page](img/png/github-new.png)
+
+Note:
+- provide a name
+- description is a good idea
+- Only initialize with a README and LICENSE when:
+    - you **do not** intend to upload an existing project
